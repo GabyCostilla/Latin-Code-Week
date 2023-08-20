f3b62d632c8f9c0fcdd71de321cb8d7f40b9d808
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-08-20 12:19:24 -0300

    mejoramos inversion.js que anda medio muy bugeao

--- src/inversion.js
+++ src/inversion.js
@@ -4,11 +4,21 @@ import "./inversion.css";
 function Inversion() {
   const [investmentAmount, setInvestmentAmount] = useState(0);
   const [resultMessage, setResultMessage] = useState("");
+  const [investmentHistory, setInvestmentHistory] = useState([]);
 
   const handleInvest = () => {
     const randomPercentage = Math.random() * 20 - 10; // Valor aleatorio entre -10% y 10%
     const earnedAmount = (investmentAmount * randomPercentage) / 100;
-    
+
+    const investment = {
+      amount: investmentAmount,
+      percentage: earnedAmount,
+      timestamp: new Date().toLocaleString(),
+    };
+
+    const updatedHistory = [...investmentHistory, investment];
+    setInvestmentHistory(updatedHistory);
+
     if (earnedAmount >= 0) {
       setResultMessage(`¡Ganaste ${earnedAmount.toFixed(2)}% de tu inversión!`);
     } else {
@@ -34,6 +44,16 @@ function Inversion() {
         />
         <button onClick={handleInvest}>Invertir</button>
         <p className="result-message">{resultMessage}</p>
+      </div>
+      <div className="investment-history">
+        <h3>Historial de Inversiones</h3>
+        <ul>
+          {investmentHistory.map((investment, index) => (
+            <li key={index}>
+              {`${investment.timestamp}: ${investment.amount} - ${investment.percentage.toFixed(2)}%`}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
