6fc13440be63f5fb023512b3d3764a4c2ff73313
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-08-21 18:49:35 -0300

    mas css

--- src/home.js
+++ src/home.js
@@ -14,13 +14,14 @@ function Home() {
         <p>Explora el mundo de las energías renovables</p>
         <div className="container">
           <div className="links">
-            <Link to="/solar" className="solar">Energía Solar</Link>
-            <Link to="/eolica" className="eolico">Energía Eólica</Link>
-            <Link to="/geotermica" className="geotermico">Energía Geotérmica</Link>
+            <Link to="/solar" className="button solar">Energía Solar</Link>
+            <Link to="/eolica" className="button eolico">Energía Eólica</Link>
+            <Link to="/geotermica" className="button geotermico">Energía Geotérmica</Link>
             <animated.h6 style={fadeIn}>¡Haz clic en uno de los botones para saber más!</animated.h6>
           </div>
           <div className="image-container">
-            <img src="https://www.portalsolar.com.br/_next/image?url=https%3A%2F%2Finstitucional-and-info-production.s3.us-east-2.amazonaws.com%2Fimages%2F62c812d3-77bf-471f-bd45-1393eec5b0aa%2Fenergia-solar.jpg&w=3840&q=100" width="300" height="200" alt="paneles solares" />
+            <img src="https://www.portalsolar.com.br/_next/image?url=https%3A%2F%2Finstitucional-and-info-production.s3.us-east-2.amazonaws.com%2Fimages%2F62c812d3-77bf-471f-bd45-1393eec5b0aa%2Fenergia-solar.jpg&w=3840&q=100" alt="paneles solares" />
+
           </div>
         </div>
       </header>
