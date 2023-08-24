42ec653a2ac2d00290c99d4506860772357710b8
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-08-24 16:12:37 -0300

    que paso?

--- src/solar.js
+++ src/solar.js
@@ -4,7 +4,6 @@ import SolarE from "./imgs/paneles.jpg";
 import SolarE2 from "./imgs/energiaSolar.jpg";
 
 function Solar() {
-<<<<<<< HEAD
 return (
     <div className="solar-container">
     <header className="solar-header">
@@ -47,60 +46,6 @@ return (
     </main>
     </div>
 );
-=======
-    return (
-        <div className="solar-container">
-            <header className="solar-header">
-                <h1>Energía Solar</h1>
-            </header>
-            <main className="solar-content"><br></br>
-                <img
-                    src={SolarE}
-                    className="panelesImg"
-                    alt="Energía Geotérmica"
-                    height={100}
-                    width={500}
-                />
-                <p>
-                La energía solar es una energía renovable, obtenida a partir del aprovechamiento
-                de la radiación electromagnética procedente del Sol. La radiación solar que 
-                alcanza la Tierra ha sido aprovechada por el ser humano desde la antigüedad, 
-                mediante diferentes tecnologías que han ido evolucionando. 
-                Hoy en día, el calor y la luz del Sol pueden aprovecharse por medio de diversos 
-                captadores como células fotoeléctricas. 
-                </p>
-                <h1>¿Cómo se produce?</h1>
-                <p>
-                Las células solares fotovoltaicas convierten la luz del sol 
-                directamente en electricidad por el llamado efecto fotoeléctrico, 
-                por el cual determinados materiales son capaces de absorber fotones 
-                (partículas lumínicas) y liberar electrones, generando una corriente eléctrica.
-                </p>
-                <img
-                    src={SolarE2}
-                    alt="Energía Solar"
-                    
-                />
-                <p>
-                    En Resumen:
-                Reduce significativamente la huella de carbono. Es una energía limpia 
-                que no genera gases de efecto invernadero ni contaminación durante su uso.
-                Se trata de una energía renovable y sostenible.
-                Puede calentar, algo que otras energías renovables no ofrecen.
-                No requiere de la extracción de materiales de ningún tipo  para funcionar. 
-                Esto supone un mayor ahorro.
-                La luz solar es muy abundante y el uso de paneles solares se puede 
-                aprovechar en cualquier lugar. Esto es especialmente útil en zonas en las que resulta complicado crear sistemas de cableado, por ejemplo.
-                Reduce la dependencia del exterior para este tipo de suministros.
-                Conserva los recursos naturales al disminuir la necesidad de 
-                los combustibles fósiles.
-                Permite ahorrar en el consumo energético y las facturas.
-
-                </p>
-            </main>
-        </div>
-    );
->>>>>>> 8125d1ad293c68394a6dc57fe2971dec25da4f98
 }
 
 export default Solar;
