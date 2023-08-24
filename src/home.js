a1c4fbb0a8e9c07feb4efccf7ed0b0ab2bc35dfc
Marcos-Ariza <d47264202@instituto.itr.edu.ar>
2023-08-24 15:14:44 -0300

    arreglos esteticos 1

--- src/home.js
+++ src/home.js
@@ -17,7 +17,7 @@ function Home() {
             <Link to="/solar" className="button solar">Energía Solar</Link>
             <Link to="/eolica" className="button eolico">Energía Eólica</Link>
             <Link to="/geotermica" className="button geotermico">Energía Geotérmica</Link>
-            <animated.h6 style={fadeIn}>¡Haz clic en uno de los botones para saber más!</animated.h6>
+            <p className="mini">¡Haz clic en uno de los botones para saber más!</p>
           </div>
           <div className="image-container">
             <img src="https://www.portalsolar.com.br/_next/image?url=https%3A%2F%2Finstitucional-and-info-production.s3.us-east-2.amazonaws.com%2Fimages%2F62c812d3-77bf-471f-bd45-1393eec5b0aa%2Fenergia-solar.jpg&w=3840&q=100" alt="paneles solares" />
